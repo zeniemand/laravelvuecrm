b740ad60dbd2a7bf7dc31d6f752998d036a432be
serhiy <sergey@uroboros.Dlink>
2022-04-09 15:29:00 +0300

    [lvc-7] added reusable color styles and added styles to icones

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -81,7 +81,7 @@
 
                     <li class="nav-item">
                         <router-link to="/dashboard" class="nav-link">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <i class="nav-icon fas fa-tachometer-alt blue"></i>
                             <p>
                                 Dashboard
                             </p>
@@ -90,7 +90,7 @@
 
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-cog"></i>
+                            <i class="nav-icon fas fa-cog green"></i>
                             <p>
                                 Management
                                 <i class="right fas fa-angle-left"></i>
@@ -120,7 +120,7 @@
 
                     <li class="nav-item">
                         <router-link to="/profile" class="nav-link">
-                            <i class="nav-icon fas fa-user"></i>
+                            <i class="nav-icon fas fa-user orange"></i>
                             <p>
                                 Profile
                             </p>
@@ -128,12 +128,19 @@
                     </li>
 
                     <li class="nav-item">
-                        <a href="pages/widgets.html" class="nav-link">
-                            <i class="nav-icon fas fa-power-off"></i>
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-power-off red"></i>
                             <p>
-                                Logout
+                                {{ __('Logout') }}
                             </p>
+
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
