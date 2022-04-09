7d16a57fbdc37f1241430d1b05e907b578a8eacf
serhiy <sergey@uroboros.Dlink>
2022-04-09 19:18:51 +0300

    [lvc-10] vform installed and created form for adding new user

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,21 @@ require('./bootstrap');
 
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Form from 'vform';
+import {
+    Button,
+    HasError,
+    AlertError,
+    AlertErrors,
+    AlertSuccess
+} from 'vform/src/components/bootstrap5';
+
+window.Form = Form;
+Vue.component(Button.name, Button);
+Vue.component(HasError.name, HasError);
+Vue.component(AlertError.name, AlertError);
+Vue.component(AlertErrors.name, AlertErrors);
+Vue.component(AlertSuccess.name, AlertSuccess);
 
 Vue.use(VueRouter);
 
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
     <div class="container">
 
         <div class="row mt-5">
@@ -60,7 +60,41 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                        <form @submit.prevent="login" @keydown="form.onKeydown($event)">
+                            <AlertError :form="form" />
+                            <!-- <AlertErrors :form="form" /> -->
+                            <!-- <AlertSuccess :form="form" message="Your changes have beend saved!" /> -->
+
+                            <div class="mb-3">
+                                <input id="name" v-model="form.name" type="text" name="name" class="form-control" placeholder="Name">
+                                <HasError :form="form" field="name" />
+                            </div>
+
+                            <div class="mb-3">
+                                <input id="email" v-model="form.email" type="text" name="email" class="form-control" placeholder="Email Address">
+                                <HasError :form="form" field="email" />
+                            </div>
+
+                            <div class="mb-3">
+                                <textarea id="bio" v-model="form.bio" type="text" name="bio" class="form-control" placeholder="Short bio for user (Optional)"></textarea>
+                                <HasError :form="form" field="bio" />
+                            </div>
+
+                            <div class="mb-3">
+                                <select id="type" v-model="form.type" type="text" name="type" class="form-control">
+                                    <option value="">Select User Role</option>
+                                    <option value="admin">Admin</option>
+                                    <option value="user">Standard User</option>
+                                    <option value="author">Author</option>
+                                </select>
+                                <HasError :form="form" field="type" />
+                            </div>
+
+                            <div class="mb-3">
+                                <input id="password" v-model="form.password" type="password" name="password" class="form-control" placeholder="Password">
+                                <HasError :form="form" field="password" />
+                            </div>
+                        </form>
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
@@ -75,6 +109,18 @@
 
 <script>
 export default {
+    data(){
+        return {
+            form: new Form({
+                name: '',
+                email: '',
+                password: '',
+                type: '',
+                bio: '',
+                photo: ''
+            })
+        }
+    },
     name: "Users"
 }
 </script>
