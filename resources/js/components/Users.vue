5002683bf65cf4704d7d98227cc599414c90f2d8
serhiy <sergey@uroboros.Dlink>
2022-04-14 15:58:46 +0300

    [lvc-19] implemented userDelete func

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -37,7 +37,7 @@
                                         <i class="fa fa-edit blue"></i>
                                     </a>
                                     /
-                                    <a href="#">
+                                    <a href="#" @click="deleteUser(user.id, user.name)">
                                         <i class="fa fa-trash red"></i>
                                     </a>
                                 </td>
@@ -133,6 +133,32 @@ export default {
     },
     name: "Users",
     methods: {
+        deleteUser(id, name) {
+            Swal.fire({
+                title: `Are you sure to delete user: ${name} ?`,
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.form.delete('api/user/' + id).then(() => {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your file has been deleted.',
+                            'success'
+                        );
+                        Fire.$emit('AfterCreate');
+                    }).catch(error => {
+                        Swal.showValidationMessage(
+                            `Request failed: ${error}`
+                        );
+                    });
+                }
+            })
+        },
         showModalwin(){
             this.showModal = true;
         },
